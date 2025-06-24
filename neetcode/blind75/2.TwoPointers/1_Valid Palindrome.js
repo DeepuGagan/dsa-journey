f52692847afb1dda1a3fc1a7a0a9802937c0e19d
DeepuGagan <gagandeepu717@gmail.com>
2025-06-24 22:21:15 +0530

    Add Valid Palindrome solution - Blind 75 Two Pointers #1

---
+++ neetcode/blind75/2.TwoPointers/1_Valid Palindrome.js
@@ -0,0 +1,43 @@
+`https://leetcode.com/problems/valid-palindrome/`
+
+
+//my method
+const isAlphaNumeric = (char) => (
+  (char >= 'a' && char <= 'z') ||
+  (char >= 'A' && char <= 'Z') ||
+  (char >= '0' && char <= '9')
+)
+
+const IP = (str) => {
+  let filtered = ''
+  for (let s of str) {
+    isAlphaNumeric(s) && (filtered += s.toLowerCase()) // or a if/else block will work too
+  }
+ return filtered === filtered.split('').reverse().join('')
+}
+
+
+
+// DSA
+
+const IP2 = (str) => {
+  let l = 0
+  let r = str.length - 1
+  while (l < r) {
+    if (!isAlphaNumeric(str[l])) {
+      l++
+      continue //very imp
+    }
+    if (!isAlphaNumeric(str[r])) {
+      r--
+      continue //very imp
+    }
+
+    if (str[l].toLowerCase() !== str[r].toLowerCase()) {
+      return false
+    }
+    l++
+    r--
+  }
+  return true
+}
