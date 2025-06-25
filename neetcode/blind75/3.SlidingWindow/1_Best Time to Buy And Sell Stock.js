e3310a1a3c200b57455594668eebb0bed56b9831
DeepuGagan <gagandeepu717@gmail.com>
2025-06-25 23:08:13 +0530

    Add Best Time to Buy And Sell Stock solution - Blind 75 Sliding Window #1

---
+++ neetcode/blind75/3.SlidingWindow/1_Best Time to Buy And Sell Stock.js
@@ -0,0 +1,18 @@
+`https://leetcode.com/problems/best-time-to-buy-and-sell-stock/`
+
+// my method
+
+const BTTBASS = (prices) => {
+  let min = prices[0]
+  let maxProfit = 0
+
+  for (let price of prices) { //use for i for speed
+   min = Math.min(min,price)        
+   // instead of min --> if(prices[i] < min) min = price
+    maxProfit = Math.max(maxProfit, price - min) 
+     // instead of max--> 
+     // profit = price-min
+     //  if(profit > maxProfit) maxProfit = profit
+  }
+  return maxProfit
+}
