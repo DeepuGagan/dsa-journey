4f1e87dce34e717c16c04ada5d2a00979e43be75
DeepuGagan <gagandeepu717@gmail.com>
2025-07-02 23:56:48 +0530

    adding stack

---
+++ neetcode/blind75/4.Stacks/0_Stack.js
@@ -0,0 +1,44 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  // Push an item onto the stack
+  push(element) {
+    this.items.push(element);
+  }
+
+  // Pop an item off the stack
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    }
+    return this.items.pop();
+  }
+
+  // Peek at the top item
+  peek() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // Get the size of the stack
+  size() {
+    return this.items.length;
+  }
+
+  // Print the stack
+  printStack() {
+    return this.items.join(" ");
+  }
+}
+
+// Example usage
+const stack = new Stack();
