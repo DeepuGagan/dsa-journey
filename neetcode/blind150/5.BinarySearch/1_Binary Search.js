1cb6f9e6503b247cbbf1dcba5c29340cbe5a05ef
DeepuGagan <gagandeepu717@gmail.com>
2025-07-06 10:52:40 +0530

    Add Binary Search - Blind 150 Binary Search #1

---
+++ neetcode/blind150/5.BinarySearch/1_Binary Search.js
@@ -0,0 +1,32 @@
+`https://leetcode.com/problems/binary-search/description/`
+
+const BS = (nums, target) => {
+  let l = 0
+  let r = nums.length - 1
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2) // l+(r-l)/2
+    if (nums[m] === target) return m
+    if (target < nums[m]) { //use target 1st for better understanding,
+      r = m - 1             // if t<m, then t lies in left
+    } else {
+      l = m + 1
+    }
+  }
+  return -1
+}
+
+const BS_r = (l, r, nums, target) => {
+  let m = Math.floor((l + r) / 2)
+  if (l > r) return -1
+  if (nums[m] === target) return m
+  if (target < nums[m]) {
+    return BS_r(l, m - 1, nums, target)
+  } else {
+    return BS_r(m + 1, r, nums, target)
+  }
+
+}
+
+const recursion = (nums, target) => {
+  return BS_r(0, nums.length - 1, nums, target)
+}
