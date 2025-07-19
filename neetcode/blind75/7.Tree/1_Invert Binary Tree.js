0a622917734185dde432b496fcb68af45936e70b
DeepuGagan <gagandeepu717@gmail.com>
2025-07-19 10:52:40 +0530

    Add Invert Binary Tree - Blind 75 Tree #1

---
+++ neetcode/blind75/7.Tree/1_Invert Binary Tree.js
@@ -0,0 +1,19 @@
+`https://leetcode.com/problems/invert-binary-tree/`
+
+
+/**
+✅ What You Can Say in an Interview
+"I’ll start with recursive DFS since it’s clean and natural for this problem. But if this were a massive unbalanced tree or we hit stack issues, I’d switch to an iterative version with a stack."
+
+"This problem asks for the shortest path, so BFS is better since it finds that in unweighted trees/graphs."
+ */
+
+
+const IBT = (root) => {
+  if (!root) return null
+
+  [root.left, root.right] = [root.right, root.left] //also can be done via temp
+  IBT(root.left)
+  IBT(root.right)
+  return root
+}
