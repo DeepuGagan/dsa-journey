49515c72db681a0be0e5b8783d7930f37729ed8b
DeepuGagan <gagandeepu717@gmail.com>
2025-07-26 10:52:40 +0530

    Add Construct Binary Tree From Preorder And Inorder Traversal - Blind 75 Tree #9

---
+++ neetcode/blind75/7.Tree/9_Construct Binary Tree From Preorder And Inorder Traversal.js
@@ -0,0 +1,36 @@
+`https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/`
+
+
+const CBSTFPAIT = (preorder, inorder) => {
+  // Map value -> index in inorder for quick lookup
+  let inOrderMap = new Map()
+  for (let i = 0; i < inorder.length; i++) {
+    inOrderMap.set(inOrderMap[i], i)
+  }
+
+  let preorderIndex = 0
+  const arrayToTree = (left, right) => {
+    if (left > right) {
+      return null // no elements to construct
+    }
+    // Root value from preorder
+    let rootVal = preorder[preorderIndex]
+    preorderIndex++
+
+    // Create the root node
+    const root = new TreeNode(rootVal)
+
+    // Split inorder into left and right subtrees
+    let inorderIndex = inOrderMap.get(rootVal)
+
+    // Build left subtree
+    root.left = arrayToTree(root, left, inorderIndex - 1)
+
+    // Build right subtree
+    root.right = arrayToTree(root, inorderIndex + 1, right)
+
+    return root //imp
+  }
+
+  return arrayToTree(0, inorder.length - 1)
+}
